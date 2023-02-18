8221c1cff0d0165c74e4d0f10fe24caad35e567e
pravin0428 <mohite46199@gmail.com>
2023-02-19 00:20:12 +0530

    backend in progress

--- server/index.js
+++ server/index.js
@@ -1,26 +1,26 @@
 const express = require('express')
-const bodyParaser = require("body-parser")
+const bodyParser = require("body-parser")
 const mongoose = require("mongoose")
 const cors = require("cors")
 const dotenv = require("dotenv")
 const helmet = require("helmet")
-const morgon = require("morgan")
-const clientRoutes = require("./routes/client.js") 
-const generalRoutes = require("./routes/general.js") 
-const managementRoutes = require("./routes/management.js") 
-const salesRoutes = require("./routes/sales.js") 
+const morgan = require("morgan")
+const clientRoutes = require("./routes/client") 
+const generalRoutes = require("./routes/general") 
+const managementRoutes = require("./routes/management") 
+const salesRoutes = require("./routes/sales") 
+
 
 //Configuration
-dotenv.config()
-const app = express()
-app.use(express.json())
-app.use(helmet()) 
-   // we are using helmet for url instade of this ---> // app.use(express.urlencoded({extended : true}))
-app.use(helmet.crossOriginEmbedderPolicy({policy : "cross-origin"})) 
-app.use(morgon("common"))
-app.use(bodyParaser.json())
-app.use(bodyParaser.urlencoded({extended : false}))
-app.use(cors())
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
 
 
 /* Routes */
@@ -29,4 +29,18 @@ app.use("/general" , generalRoutes)
 app.use("/management" , managementRoutes)
 app.use("/sales" , salesRoutes)
 
-app.listen(PORT_NO , () =>  {console.log('listening on http://localhost:8080')})
+/* Mongoose setup */
+
+const PORT = process.env.PORT_NO || 9000
+
+mongoose.connect(process.env.DB_URL , {
+    useNewUrlParser: true,
+ 
+  
+}).then((res) => {
+    app.listen(PORT , () => {
+        console.log(`server stated on http://localhost:${PORT}`);
+    })
+}).catch((error)=>{
+    console.log(`${error} failed to connet the database`);
+})
